5af73e97a2011080d28f8f0e05ebcdb9fbca1a1a
t-anvirahmed <t.anvirahmed@outlook.com>
2026-09-19 10:39:45 +0800

    added navbar

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,80 @@
+import Logo from "../assets/logo-text.png";
+import "../index.css";
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full z-50">
+      <div className="container mx-auto flex justify-between items-center p-4">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                aria-label="Menu"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-primary-text"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Techologies</a>
+              </li>
+              <li>
+                <a>Projects</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+            </ul>
+          </div>
+          <img src={Logo} alt="" />
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-primary-text">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Techologies</a>
+            </li>
+            <li>
+              <a>Projects</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end gap-6">
+          <button className="text-primary-text">Sign in</button>
+          <button className="btn border-none rounded-4xl text-white bg-nav-btn">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
